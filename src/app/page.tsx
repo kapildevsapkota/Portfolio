49572a1ae3b0e8936ae60a882a6d59e3a801d1ec
kapil dev sapkota <kapildevsapkota98@gmail.com>
2025-07-29 00:27:50 +0545

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,14 @@ import { ContactSection } from "@/components/contact/contact";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      {/* Responsive Container */}
-      <div className="w-full px-4 sm:px-6 lg:px-8 xl:px-16 2xl:px-20">
-        <div className="max-w-7xl">
-          <HeroSection />
-          <AboutSection />
-          <ExperienceSection />
-          <PortfolioSection />
-          <ServicesSection />
-          <BlogSection />
-          <ContactSection />
-        </div>
-      </div>
+    <main>
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <PortfolioSection />
+      <ServicesSection />
+      <BlogSection />
+      <ContactSection />
     </main>
   );
 }
